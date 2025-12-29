e6991ce9fcacb0c1364b95f0514973141471af64
Kwan  Suet Ling <kwansue1@ug145.eecg.toronto.edu>
2025-12-29 13:56:15 -0500

    feat: feed input from parent to child, todo: loop input

--- harmonic/exercise.cpp
+++ harmonic/exercise.cpp
@@ -7,28 +7,46 @@ using namespace std;
 int main() {
     
     // declare pipe
-    int p[2]; 
+    int p[2]; // child writes parent 
+    int fd[2]; // parent writes child
     char* buf[10]; 
     pipe(p);
+    pipe(fd);
     pid_t pid = fork(); 
 
     if (pid == 0) { // child process
         // launch the child process
         char *argv[] = {NULL}; 
         // declare pipe here -> write to stdout
+
+        // read port
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]); 
+
+        // write port
         close(p[0]);
-        dup2(p[1],STDOUT_FILENO); 
+        dup2(p[1],STDOUT_FILENO); // write into the pipe (std output no longer be the output -> outputs to the pipe)
         close(p[1]);
+
         execv("harmonic",argv); 
         exit(1); 
+
     } else { // parent process
-        // close(p[1]);
+
+        // write input to child
+        close(fd[0]); 
+        write(fd[1],"2\n",sizeof("2\n"));
+        close(fd[1]);  
+
+        // write pipe 
+        close(p[1]);
         close(p[1]); // close write pipe - because the read is trying to get as much data as possible as long as the write port is open
         while (read(p[0],buf,sizeof(buf)) > 0) {
             printf("%s",buf);
         }
-        
-        waitpid(pid,0,0); 
+
+        // waitpid(pid,0,0); 
         
         printf("parent done\n");
         // get the output from the stdout -> declare pipe here 
